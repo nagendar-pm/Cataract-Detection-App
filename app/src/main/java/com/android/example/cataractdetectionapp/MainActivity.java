91f168328bb316930d741e78ffa0d606e753fdeb
nagendar-pm <nagender.pm48@gmail.com>
2021-09-19 10:30:06 +0530

    tflite initial commit

--- app/src/main/java/com/android/example/cataractdetectionapp/MainActivity.java
+++ app/src/main/java/com/android/example/cataractdetectionapp/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         createDirectory();
 
         Button submit = findViewById(R.id.submit);
